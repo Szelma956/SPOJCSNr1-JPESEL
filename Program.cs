2183137494d1bea24a17d09eb01470ee7e95fc33
Szelma956 <w.bielinski1@gmail.com>
2022-07-11 11:37:01 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace JPESEL
+{
+
+    class Pesel
+    {
+
+        public int[] cyfry;
+
+
+        public bool IsValid()
+        {
+            int[] wagi = new int[]
+            {
+                1,3,7,9,1,3,7,9,1,3,1
+            };
+
+            int suma = 0;
+
+            for (int i = 0; i < 11; i++)
+            {
+
+                suma += cyfry[i] * wagi[i];
+
+            }
+
+            return suma % 10 == 0;
+
+        }
+
+
+        public static Pesel Parse(string str)
+        {
+            int[] cyfry = new int[11];
+
+            for (int i = 0; i < 11; i++)
+            {
+
+                char znak = str[i];
+                int cyfra = znak - '0';
+                cyfry[i] = cyfra;
+
+            }
+
+            Pesel pesel = new Pesel();
+            pesel.cyfry = cyfry;
+
+
+            return pesel;
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linia = Console.ReadLine();
+            int liczbaPrzypadkow = int.Parse(linia);
+
+            for (int i = 0; i < liczbaPrzypadkow; i++)
+            {
+
+                string peselstr = Console.ReadLine();
+                Pesel pesel = Pesel.Parse(peselstr);
+                if (pesel.IsValid())
+                {
+
+                    Console.WriteLine('D');
+
+                }
+                else {
+
+                    Console.WriteLine('N');
+                
+                }
+
+
+
+            }
+        }
+    }
+}
